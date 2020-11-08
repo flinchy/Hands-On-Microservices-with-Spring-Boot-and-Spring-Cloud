7ac23e656ca56c0f5e08d6adff28be390d498d84
chisom <iwowochisom@gmail.com>
2020-11-09 00:23:44 +0100

    add all microservice projects

---
+++ chapter03/1-spring-init/microservices/review-service/src/main/java/com/chisom/microservices/review/reviewservice/ReviewServiceApplication.java
@@ -0,0 +1,13 @@
+package com.chisom.microservices.review.reviewservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewServiceApplication.class, args);
+	}
+
+}
---
+++ chapter03/1-spring-init/microservices/review-service/src/test/java/com/chisom/microservices/review/reviewservice/ReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chisom.microservices.review.reviewservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
